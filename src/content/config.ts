0537825f79773bc075cb253e2fd24794c9c0c9d9
anon <anon@anon.com>
2024-09-10 05:04:12 -0600

    remove unused navigation items

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { z, defineCollection } from 'astro:content';
 
 export const collections = {
-	blog: defineCollection({
+	planets: defineCollection({
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
@@ -13,27 +13,16 @@ export const collections = {
 		}),
 
 	}),
-	soulpreneurs: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
 
-	}),
-	event: defineCollection({
+	extraterrestrials: defineCollection({
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
-			sortDate: z.coerce.date(),
-			eventDate: z.string(),
-			eventTime: z.string(),
-			eventLocation: z.string(),
 		}),
 
 	}),
